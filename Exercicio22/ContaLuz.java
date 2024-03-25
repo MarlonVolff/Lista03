eb3a77b2f27f0a88c853a707e27d28af56a88aa9
MarlonVolff <marlonvolffsilva@gmail.com>
2024-03-25 16:07:55 -0300

    Exercicio22

--- Exercicio22/ContaLuz.java
+++ Exercicio22/ContaLuz.java
@@ -1,1 +1,19 @@
 package Exercicio22;
+
+public class ContaLuz{
+    public int cliente;
+    public double qtdKwh;
+
+    public ContaLuz(int cliente, double qtdKwh){
+        this.cliente = cliente;
+        this.qtdKwh = qtdKwh;
+    }
+    public int getCliente(){
+        return cliente;
+    }
+    public double getQtdKwh(){
+        return qtdKwh;
+    }
+    public ContaLuz() {
+    }
+}
